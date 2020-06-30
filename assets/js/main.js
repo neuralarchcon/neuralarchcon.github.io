932110cca12e8078897d17bd1dc97d2b98fae50a
Marcin Koculak <koculak.marcin@gmail.com>
2020-06-30 21:16:29 +0200

    Updating website content
    Adding info on the leaders, reorganising the material, etc.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,74 @@
     $('#single-testimonial-item').owlCarousel({
-        items : 1,
-        navigation : true,
-        pagination: false,
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
+      items: 1,
+      navigation: true,
+      pagination: false,
+      slideSpeed: 300,
+      paginationSpeed: 400,
+      singleItem: true
     });
 
     $('#single-testimonial-item').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
     $('#single-testimonial-item').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
 
-      $('#image-carousel').owlCarousel({
-        navigation : false,
-        pagination: true,
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
+    $('#image-carousel').owlCarousel({
+      navigation: false,
+      pagination: true,
+      items: 4,
+      itemsDesktop: [1199, 3],
+      itemsDesktopSmall: [979, 3]
+    });
+
+    $('#posts-carousel').owlCarousel({
+      navigation: true,
+      pagination: true,
+      items: 4,
+      itemsDesktop: [1199, 3],
+      itemsDesktopSmall: [979, 3]
+    });
+
+    $('#posts-carousel').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
+    $('#posts-carousel').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+
+
+    $('#posts-carousel-3col').owlCarousel({
+      navigation: true,
+      pagination: false,
+      items: 3,
+      itemsDesktop: [1199, 3],
+      itemsDesktopSmall: [979, 3]
+    });
+
+    $('#posts-carousel-3col').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
+    $('#posts-carousel-3col').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+
+    $('#image-slider').owlCarousel({
+      navigation: true, // Show next and prev buttons
+      slideSpeed: 300,
+      paginationSpeed: 400,
+      singleItem: true
+    });
+    $('#image-slider').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
+    $('#image-slider').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+
+
+    // Navbar code
+    $(function() {
+      // ------------------------------------------------------- //
+      // Multi Level dropdowns
+      // ------------------------------------------------------ //
+      $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        $(this).siblings().toggleClass("show");
+
+
+        if (!$(this).next().hasClass('show')) {
+          $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+          $('.dropdown-submenu .show').removeClass("show");
+        });
+
       });
-
-      $('#posts-carousel').owlCarousel({
-        navigation : true,
-        pagination: true,
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-      });
-
-      $('#posts-carousel').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-      $('#posts-carousel').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
-
-
-      $('#posts-carousel-3col').owlCarousel({
-        navigation : true,
-        pagination: false,
-        items : 3,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
-      });
-
-      $('#posts-carousel-3col').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-      $('#posts-carousel-3col').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
-
-      $('#image-slider').owlCarousel({
-          navigation : true, // Show next and prev buttons
-          slideSpeed : 300,
-          paginationSpeed : 400,
-          singleItem:true
-      });
-      $('#image-slider').find('.owl-prev').html('<i class="fa fa-angle-left"></i>');
-      $('#image-slider').find('.owl-next').html('<i class="fa fa-angle-right"></i>');
+    });
